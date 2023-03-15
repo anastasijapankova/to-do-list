faf2e6af5bc8a75f98310c916cb86ed0ca3b95d0
Anh Chau <anhchau8922@gmail.com>
2023-03-15 15:20:08 +0000

    popup added

--- anh.js
+++ anh.js
@@ -4,12 +4,22 @@ function accountPopup2() {
     document.getElementById("popUp2").classList.toggle("show");
   }
 
-
 function accountPopup1() {
     document.getElementById("popUp1").classList.toggle("show");
   }
 
+
+
+function addBoard() {
+    document.getElementById("popUp3").classList.toggle("show");
+}
+
+var loginpopup = document.getElementById('login001');
+
+// When the user clicks anywhere outside of the login box, close it
+
 // Close the dropdown if the user clicks outside of it
+
   window.onclick = function(event) {
     if (!event.target.matches('.drop-btn')) {
       var droppopup2 = document.getElementsByClassName("drop-account");
@@ -27,6 +37,10 @@ function accountPopup1() {
     console.log('yo', event.target)
     const el = document.querySelector(".bell-notify")
     if (el.classList.contains('show')) el.classList.remove('show') 
+  }
+
+  if (event.target == loginpopup) {
+    loginpopup.style.display = "none";
   }
 
   }
